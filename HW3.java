788ce5cafdeccb9c9e36c6c6ed6d0e92eea6500b
YiJu Tseng <beergt@gmail.com>
2016-10-16 17:12:57 +0800

    HW3

---
+++ HW3.java
@@ -0,0 +1,80 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+//import java.util.Scanner;
+public class HW3 {
+	public static void main(String[] args) {
+		//Scanner sc = new Scanner(System.in);
+		//System.out.println("input N(deck of cards):");
+		//String testn= sc.nextLine(); 
+		int nDeck=1;
+		Deck deck=new Deck(nDeck);
+		//TODO: please check your output, make sure that you print all cards on your screen
+		Card newCard=deck.getOneCard();
+		newCard.printCard();
+		deck.shuffle();
+		Card newCard2=deck.getOneCard();
+		newCard2.printCard();
+		if(isAllCardsCorrect(deck.getAllCards(),nDeck)){
+			if(!isShuffleWorking(deck,newCard,newCard2)){
+				System.out.println("All Card: Well done! But shufller is not working");
+			}else{
+				System.out.println("Well done!");
+			}
+			
+		}else{
+			System.out.println("All Card: Error, please check your sourse code");
+		}
+
+	}
+	/*
+	 * This method is used for checking your result, not a part of your HW3
+	 */
+	private static boolean isShuffleWorking(Deck deck,Card newCard,Card newCard2){
+		boolean isCorrect=true;
+		if(newCard.getSuit().equals(newCard2.getSuit()) &&
+				newCard.getRank()==newCard2.getRank()){
+					isCorrect=false;
+					return isCorrect;
+		}
+		for(int i=0;i<53;i++){
+			deck.getOneCard();
+		}
+		if(deck.nUsed!=2){
+			isCorrect=false;
+		}
+		return isCorrect;
+	}
+	
+	private static boolean isAllCardsCorrect(ArrayList<Card> allCards,int nDeck){
+		//check the output 
+		boolean isCorrect=true;;
+		HashMap <String,Integer> checkHash=new HashMap<String,Integer>();
+		for(Card card:allCards){
+			Card.Suit suit= card.getSuit();
+			int rank = card.getRank();
+			if(rank>13||rank<1){
+				isCorrect=false;
+				break;
+			}
+			if(checkHash.containsKey(suit+","+rank)){
+				checkHash.put(suit+","+rank, 
+						checkHash.get(suit+","+rank)+1);
+			}else{
+				checkHash.put(suit+","+rank, 1);
+			}
+
+		}
+		if(checkHash.keySet().size()==52){
+			for(int value:checkHash.values()){
+				if(value!=nDeck){
+					isCorrect=false;
+					break;
+				}
+			}
+		}else{
+			isCorrect=false;
+		}
+		return isCorrect;
+	}
+
+}
