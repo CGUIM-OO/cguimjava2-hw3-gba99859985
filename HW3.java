5e8973f83d6c88ae8a20e2626cb9cf14a2ef2870
Yi-Ju Tseng <beergt@gmail.com>
2017-10-15 10:51:41 -0500

    Update HW3.java

--- HW3.java
+++ HW3.java
@@ -1,14 +1,31 @@
 import java.util.ArrayList;
 import java.util.HashMap;
 //import java.util.Scanner;
+
+/**
+ * @author TODO: please add student ID and name here
+ * Try to write some comments for your codes (methods, 10 points)
+ */
 public class HW3 {
 	public static void main(String[] args) {
 		//Scanner sc = new Scanner(System.in);
 		//System.out.println("input N(deck of cards):");
 		//String testn= sc.nextLine(); 
 		int nDeck=1;
+		//TODO: please add new fields and methods to Deck class (35)
+		//usedCard (5 points)
+		//nUsed (5 points)
+		//getOneCard (10 points)
+		//shuffle (10 points)
+		//constructor (5 points)
 		Deck deck=new Deck(nDeck);
-		//TODO: please check your output, make sure that you print newCard and newCard2 on your screen
+		
+		//TODO: please check your output, make sure that you print newCard and newCard2 on your screen  (10 points)
+		//TODO: please add new fields and methods to Card class (25)
+		//Use enumerated type in Card (10 points)
+		//Constructor (5 points)
+		//printCard (5 points)
+		//getSuit (5 points)
 		Card newCard=deck.getOneCard();
 		newCard.printCard();
 		Card newCard2=deck.getOneCard();
@@ -26,7 +43,7 @@ public class HW3 {
 		}
 
 	}
-	/*
+	/**
 	 * This method is used for checking your result, not a part of your HW3
 	 */
 	private static boolean isShuffleWorking(Deck deck,Card newCard,Card newCard2){
